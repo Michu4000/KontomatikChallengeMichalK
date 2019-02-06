30a64bd6b34a11e440cb90b6ff22aa689c9fd126
Michu4000 <michal-kr@o2.pl>
2019-02-06 11:57:46 +0100

    fix WebClientFactory
    fix WebClientFactory to be threadsafe

--- src/main/java/michu4k/kontomatikchallenge/utils/WebClientFactory.java
+++ src/main/java/michu4k/kontomatikchallenge/utils/WebClientFactory.java
@@ -4,29 +4,18 @@ import com.gargoylesoftware.htmlunit.ProxyConfig;
 import com.gargoylesoftware.htmlunit.WebClient;
 
 public class WebClientFactory {
-    //TODO remove static webClient - it's not thread safe
-    private static WebClient webClient;
-
     public static WebClient getWebClient() {
-        createAndSetWebClient();
+        WebClient webClient = new WebClient();
+        webClient.getOptions().setJavaScriptEnabled(false);
+        webClient.getOptions().setCssEnabled(false);
         return webClient;
     }
 
     public static WebClient getWebClientWithProxy() {
-        createAndSetWebClientWithProxy();
-        return webClient;
-    }
-
-    private static void createAndSetWebClient() {
-        webClient = new WebClient();
-        webClient.getOptions().setJavaScriptEnabled(false);
-        webClient.getOptions().setCssEnabled(false);
-    }
-
-    private static void createAndSetWebClientWithProxy() {
-        createAndSetWebClient();
+        WebClient webClient = getWebClient();
         ProxyConfig proxyConfig = new ProxyConfig("127.0.0.1", 8080);
         webClient.getOptions().setProxyConfig(proxyConfig);
         webClient.getOptions().setUseInsecureSSL(true);
+        return webClient;
     }
 }
